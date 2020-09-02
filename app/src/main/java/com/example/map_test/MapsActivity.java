8f92e0dfb5326fd97c2cb7fb5a52e828860855a3
d3da <s2014203@utwente.nl>
2020-09-02 12:34:45 +0200

    Initial commit

---
+++ app/src/main/java/com/example/map_test/MapsActivity.java
@@ -0,0 +1,156 @@
+package com.example.map_test;
+
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.os.Looper;
+import android.util.Log;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, SensorEventListener {
+
+    // Maps
+    private GoogleMap mMap;
+
+    // Location services
+    private boolean requestingLocationUpdates = false;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private LocationCallback locationCallback;
+
+    // Sensor services
+    private SensorManager sensMgr;
+    private Sensor linearAccelSensor;
+    private Sensor magneticFieldSensor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //// From template
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        //////////////////////////////////////////////////
+        /// Start location services
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        if (!requestingLocationUpdates) {
+            startLocationUpdates();
+        }
+
+        /// Start Sensor mgmt
+        sensMgr = (SensorManager) getSystemService(SENSOR_SERVICE);
+        linearAccelSensor = sensMgr.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        magneticFieldSensor = sensMgr.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+//        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (!requestingLocationUpdates) {
+            startLocationUpdates();
+        }
+
+        sensMgr.registerListener(this, linearAccelSensor, SensorManager.SENSOR_DELAY_FASTEST);
+
+    }
+
+    private void startLocationUpdates() {
+
+        LocationRequest locationRequest = LocationRequest.create();
+        long locationUpdateInterval = 1000;
+        locationRequest.setInterval(locationUpdateInterval);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) {
+                    return;
+                }
+                for (Location location : locationResult.getLocations()) {
+                    if (location != null) {
+                        double wayLatitude = location.getLatitude();
+                        double wayLongitude = location.getLongitude();
+                        Log.e(this.getClass().toString(), String.format("%f, %f", wayLatitude, wayLongitude));
+
+                        LatLng loc = new LatLng(wayLatitude, wayLongitude);
+                        mMap.addMarker(new MarkerOptions().position(loc).title("My Location"));
+                        mMap.moveCamera(CameraUpdateFactory.newLatLng(loc));
+                    }
+                }
+            }
+        };
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 420);
+            return;
+        }
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper());
+    }
+
+    private void startSensorUpdates(){
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        int accuracy = sensorEvent.accuracy;
+        long timestamp = sensorEvent.timestamp;
+        float xVal = sensorEvent.values[0];
+        float yVal = sensorEvent.values[1];
+        float zVal = sensorEvent.values[2];
+
+
+
+        Log.e(this.getClass().toString(), String.format("time: %d\tacc: %d\tvalues: %d / %d/ %d", timestamp, accuracy, xVal, yVal, zVal));
+
+
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        Log.e(this.getClass().toString(), String.format("Accuracy changed"));
+
+    }
+}
